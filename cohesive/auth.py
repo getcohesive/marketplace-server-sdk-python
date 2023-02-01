1b9ae591fafb3c028ec9ab68c0ac29b589f36017
vaibhav.sinha <vaibhav.sinha@unacademy.com>
2023-02-01 21:03:56 +0530

    Explicit init

--- cohesive/auth.py
+++ cohesive/auth.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-import datetime
 
 import jwt
 import cohesive
@@ -15,14 +14,24 @@ class AuthDetails:
     workspace_id: int
     workspace_name: str
     instance_id: int
-    current_period_started_at: datetime.datetime
-    current_period_ends_at: datetime.datetime
+    current_period_started_at: str
+    current_period_ends_at: str
     is_in_trial: bool
 
 
 def validate_token(token: str) -> AuthDetails:
     try:
         claims = jwt.decode(token, cohesive.app_secret, algorithms=["HS256"])
-        return AuthDetails(**claims)
+        return AuthDetails(
+            user_id=claims.get("user_id"),
+            user_name=claims.get("user_name"),
+            role=claims.get("role"),
+            workspace_id=claims.get("workspace_id"),
+            workspace_name=claims.get("workspace_name"),
+            instance_id=claims.get("instance_id"),
+            current_period_started_at=claims.get("current_period_started_at"),
+            current_period_ends_at=claims.get("current_period_ends_at"),
+            is_in_trial=claims.get("is_in_trial"),
+        )
     except jwt.exceptions.PyJWTError as e:
         raise AuthenticationError(message=str(e), http_status=None, http_body=None, http_headers=None)
